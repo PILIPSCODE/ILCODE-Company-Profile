0a3e1b96c1e68f450c7f1ae940f7ef651af3ff67
Pilipus <Pilipus@gmail.com>
2025-06-01 10:14:58 +0700

    fix: animation not run when refresh

--- script.js
+++ script.js
@@ -8,25 +8,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => {
   initScroll();
+
+  setTimeout(() => {
+    ScrollTrigger.refresh();
+  }, 2200);
 });
 
 function initScroll() {
   const wrapper = document.querySelector(".wrapper");
   const card = document.querySelectorAll(".card");
 
-  // 1. Set initial positions (below the first card)
   card.forEach((el, i) => {
     if (i !== 0) {
       gsap.set(el, { yPercent: 130 });
     }
   });
 
-  // 2. Create timeline AFTER setting yPercent
   const timeline = gsap.timeline({
     scrollTrigger: {
       trigger: wrapper,
       pin: true,
-      start: "top 150px", // your custom offset
+      start: "top 150px",
       end: () => `+=${card.length * 100}%`,
       scrub: 1,
     },

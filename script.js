415f40fc3b8535fe948114894ff5c6627e7b7b31
Pilipus <Pilipus@gmail.com>
2025-06-12 20:25:34 +0700

    fix: seperated js allpage

--- script.js
+++ script.js
@@ -54,65 +54,10 @@ gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => {
   setTimeout(() => {
-    initScroll();
     setupFadeUpScroll();
     ScrollTrigger.refresh();
   }, 2200);
 });
-
-// Project Card Stacked Scroll
-function initScroll() {
-  const wrapper = document.querySelector(".wrapper");
-  const card = document.querySelectorAll(".card");
-
-  card.forEach((el, i) => {
-    gsap.set(el, { y: 0 });
-    if (i !== 0) {
-      gsap.set(el, { yPercent: 100 });
-    }
-  });
-
-  const timeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: wrapper,
-      pin: true,
-      start: "top 300px",
-      end: () => `+=${card.length * 100}%`,
-      scrub: 1,
-    },
-    defaults: { ease: "none" },
-  });
-
-  card.forEach((_, i) => {
-    timeline.to(card[i], { y: -220 });
-    if (i < card.length - 1) {
-      timeline.to(card[i + 1], { yPercent: 0 });
-    }
-  });
-
-  // Project Horizontal Scroll
-  const wrapperHorizontal = document.querySelector(".wrapper-horizontal");
-  const scrollHorizontal = document.querySelector(".scroll-horizontal");
-  const cardHorizontal = document.querySelectorAll(".card-horizontal");
-
-  gsap.set(scrollHorizontal, { xPercent: 0 });
-
-  const timelineHorizontal = gsap.timeline({
-    scrollTrigger: {
-      trigger: wrapperHorizontal,
-      pin: true,
-      start: "top top",
-      end: () =>
-        `+=${cardHorizontal.length * wrapperHorizontal.offsetHeight}px`,
-      scrub: 1,
-    },
-    defaults: { ease: "none" },
-  });
-
-  timelineHorizontal.to(scrollHorizontal, {
-    xPercent: -100 * (cardHorizontal.length - 1),
-  });
-}
 
 function setupFadeUpScroll() {
   gsap.registerPlugin(ScrollTrigger);
@@ -130,11 +75,11 @@ function setupFadeUpScroll() {
 
     gsap.to(span, {
       bottom: 0,
-      duration: 2,
+      duration: 10,
       scrollTrigger: {
         trigger: el,
-        start: "-20% bottom",
-        end: "bottom 80%",
+        start: "top center",
+        end: "bottom center",
         scrub: true,
         id: `fadeUp-${index}`,
       },
@@ -178,34 +123,3 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
-
-// form
-
-function validate (inputElement, errorElement){
-  if (inputElement.value.trim() === ""){
-    inputElement.classList.add("error");
-    errorElement.textContent = "ndak boleh kosong"
-    return false;
-  } else {
-    inputElement.classList.remove("error");
-    errorElement.textContent = ""
-    return true;
-  }
-}
-
-const form = document.getElementById("form");
-form.addEventListener("submit", function(e){
-  e.preventDefault();
-
-  const namaInput = document.getElementById("username");
-  const emailInput = document.getElementById("email");
-  const errorName = document.getElementById("errorName")
-  const errorEmail = document.getElementById("errorEmail")
-
-  const namaValid = validate(namaInput, errorName);
-  const emailValid = validate(emailInput, errorEmail);
-
-  if(namaValid && emailValid){
-    form.submit()
-  }
-})
---
+++ script/home.js
@@ -0,0 +1,62 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    initScroll();
+    ScrollTrigger.refresh();
+  }, 2200);
+});
+
+// Project Card Stacked Scroll
+function initScroll() {
+  const wrapper = document.querySelector(".wrapper");
+  const card = document.querySelectorAll(".card");
+
+  card.forEach((el, i) => {
+    gsap.set(el, { y: 0 });
+    if (i !== 0) {
+      gsap.set(el, { yPercent: 100 });
+    }
+  });
+
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: wrapper,
+      pin: true,
+      start: "top 300px",
+      end: () => `+=${card.length * 100}%`,
+      scrub: 1,
+    },
+    defaults: { ease: "none" },
+  });
+
+  card.forEach((_, i) => {
+    timeline.to(card[i], { y: -220 });
+    if (i < card.length - 1) {
+      timeline.to(card[i + 1], { yPercent: 0 });
+    }
+  });
+
+  // Project Horizontal Scroll
+  const wrapperHorizontal = document.querySelector(".wrapper-horizontal");
+  const scrollHorizontal = document.querySelector(".scroll-horizontal");
+  const cardHorizontal = document.querySelectorAll(".card-horizontal");
+
+  gsap.set(scrollHorizontal, { xPercent: 0 });
+
+  const timelineHorizontal = gsap.timeline({
+    scrollTrigger: {
+      trigger: wrapperHorizontal,
+      pin: true,
+      start: "top top",
+      end: () =>
+        `+=${cardHorizontal.length * wrapperHorizontal.offsetHeight}px`,
+      scrub: 1,
+    },
+    defaults: { ease: "none" },
+  });
+
+  timelineHorizontal.to(scrollHorizontal, {
+    xPercent: -100 * (cardHorizontal.length - 1),
+  });
+}
